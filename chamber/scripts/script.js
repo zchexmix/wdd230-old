294a5c4e672684d2805d6b6987f844d04ade4751
zchexmix <zachchecketts9@gmail.com>
2023-10-22 15:14:19 +0100

    Home Page fixes

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -13,3 +13,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+  navigation.classList.toggle('open');
+  hamButton.classList.toggle('open');
+});
