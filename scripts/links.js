f6c3204c1a2b1e0e25a66346d0559e7b26e48ebb
zchexmix <zachchecketts9@gmail.com>
2023-11-11 18:59:48 +0000

    comma fix

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,10 @@
 const baseURL = "https://zchexmix.github.io/wdd230/";
+const linksURL = "https://zchexmix.github.io/wdd230/data/links.json";
 
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+
+}
+getLinks();
