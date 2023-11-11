51ac2b9462d519f1fb693f217d6873ddf402b0ec
zchexmix <zachchecketts9@gmail.com>
2023-11-11 22:11:43 +0000

    minor fixes

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,5 +1,5 @@
 const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
-const cards = document.querySelector("#cards");
+const cards = document.querySelector("#assugnments");
 
 async function getProphetData() {
     const response = await fetch(url);
--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,34 @@
 const baseURL = "https://zchexmix.github.io/wdd230/";
 const linksURL = "https://zchexmix.github.io/wdd230/data/links.json";
+const cards = document.querySelector("#assignments");
+const myElement = document.getElement
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+
+    displayLinks(data.lessons);
 
 }
+
+const displayLinks = (lessons) => {
+    lessons.forEach((lesson) => {
+        const listElement = document.createElement("li");
+        const aElement = document.createElement("a");
+        aElement.setAttribute('href', lesson.links);
+        aElement.textContent(lesson.lesson);
+
+
+        cards.appendChild(listElement);
+        listElement.appendChild(assignmentList);
+
+
+
+
+    })
+
+    
+}
+
 getLinks();
